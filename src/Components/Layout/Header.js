8cbf123c81feee8ec2920b82d5957ad19434f832
Sumit Pandey <heerjnv@gmail.com>
2024-07-17 16:08:54 +0530

    Your commit message here

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <div>ReactMeal</div>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src="https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
