242744b35fd634f1a139e39a0ffc7d186dec2e0d
Sumit Pandey <heerjnv@gmail.com>
2024-07-20 21:38:47 +0530

    aaaa

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Summery from "./Summery";
+import AvailableMeal from "./AvailableMeal";
 
 const Meals = () => {
   return (
     <div>
       <Summery />
+      <AvailableMeal />
     </div>
   );
 };
